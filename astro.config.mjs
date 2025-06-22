d2e1ec148e5a339f27c844064e21cf400abe0815
David Day <dj6082013@gmail.com>
2025-06-21 20:41:01 -0700

    Use Astro.

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://davidday.tw',
+  integrations: [sitemap(), robotsTxt()]
+});
